1ea6201881bc97ace2cd4176cb1ef32ae55fded1
YUUKIToriyama <ytoriyama@torisfactory.com>
2021-06-02 22:16:00 +0900

    Update src/utils.ts - ベクトル和を求める関数を追加

--- src/utils.ts
+++ src/utils.ts
@@ -11,8 +11,19 @@ const lcm = (a: number, b: number): number => {
 	const d = gcd(a, b);
 	return b * (a / d);
 }
-
+// ベクトルの足し算
+const addTwoVectors = (left: number[], right: number[]): number[] => {
+	if (left.length !== right.length) {
+		throw Error("要素の数が異なるので足し合わせることはできません");
+	}
+	let result: number[] = [];
+	left.forEach((elem, index) => {
+		result.push(elem + right[index]);
+	});
+	return result;
+}
 export {
 	gcd,
-	lcm
+	lcm,
+	addTwoVectors
 }

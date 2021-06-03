35db8f0bc530a3b255a8fc77ef1a4ada05c1a87e
YUUKIToriyama <ytoriyama@torisfactory.com>
2021-06-03 16:17:50 +0900

    update src/bin.ts - cacを導入 - cliツールとして整備

--- src/bin.ts
+++ src/bin.ts
@@ -1,8 +1,30 @@
+#!/usr/bin/env node
+import { cac } from "cac";
 import Gyolets from "./main";
 
-const a = new Gyolets([[2, -1, 5], [0, 2, 2], [1, 0, 3]], {
-	row: 3,
-	column: 3
-}, { rapid: true, verbose: false });
-const result = a.reduction();
-result.log();
+const cli = cac("@toriyama/gyolets");
+cli.version("0.9.0");
+cli.help();
+
+cli.option("-m, --matrix <number[][]>", "行列オブジェクトに変換したい二次元配列を指定");
+cli.option("-v, --verbose", "計算過程を逐次表示します", {
+	default: false
+});
+cli.option("-r, --rapid", "途中計算を少し省略します", {
+	default: false
+});
+
+const parsed = cli.parse();
+if (parsed.options.matrix !== undefined) {
+	const array: number[][] = JSON.parse(parsed.options.matrix);
+	const mat = new Gyolets(array, {
+		row: array.length,
+		column: array[0].length
+	}, {
+		verbose: parsed.options.verbose,
+		rapid: parsed.options.rapid
+	});
+	mat.log();
+	const result = mat.reduction();
+	result.log();
+}

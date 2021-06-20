70f90fc523952ea6bee824ae59bbbea3cf36dce3
YUUKIToriyama <ytoriyama@torisfactory.com>
2021-06-20 20:05:24 +0900

    update web/modules/Header.tsx

--- web/app.tsx
+++ web/app.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import "@blueprintjs/core/lib/css/blueprint.css";
-import Header from './modules/Header';
+import Header, { ButtonProps } from './modules/Header';
 
 const App: React.FunctionComponent = () => {
+	const headerBarButtons: ButtonProps[] = [
+		{ icon: "help", text: "Docs", href: "./docs/index.html" },
+		{ icon: "git-repo", text: "GitHub", href: "https://github.com/YUUKIToriyama/gyolets" },
+		{ icon: "chat", text: "Twitter", href: "https://twitter.com/CoconMap" }
+	]
 	return (
 		<div>
-			<Header title="Gyolets" buttons={{ icon: "home", label: "HOME" }} />
+			<Header title="Gyolets" buttons={headerBarButtons} />
 			<h1>Hello, world</h1>
 			<p>hogehoge</p>
 		</div>
--- web/modules/Header.tsx
+++ web/modules/Header.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import {
 	Alignment,
+	AnchorButton,
 	IconName,
 	Navbar,
 	Button
 } from '@blueprintjs/core';
+export interface ButtonProps {
+	icon: IconName
+	text?: string
+	href?: string
+}
 export interface HeaderProps {
 	title: string
-	buttons: {
-		icon: IconName
-		label: string
-	}
+	buttons: ButtonProps[]
 }
 const Header: React.FunctionComponent<HeaderProps> = (props) => {
 	return (
-		<Navbar>
+		<Navbar className="bp3-dark">
 			<Navbar.Group align={Alignment.LEFT}>
 				<Navbar.Heading>{props.title}</Navbar.Heading>
+			</Navbar.Group>
+			<Navbar.Group align={Alignment.RIGHT}>
 				<Navbar.Divider />
-				<Button className="bp3-minimal" icon={props.buttons.icon} text={props.buttons.label} />
+				{props.buttons.map(buttonProps => {
+					return buttonProps.href === undefined ? <Button className="bp3-minimal" {...buttonProps} /> : <AnchorButton {...buttonProps} />
+				})}
 			</Navbar.Group>
 		</Navbar>
 	)
